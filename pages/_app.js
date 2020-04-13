8c15c8a311309be8ea0db507a53d082b4cfc5813
Daniel Gutierrez Duque <danielgudu22@gmail.com>
2020-04-13 10:41:11 -0500

    importing polaris and nxt config

---
+++ pages/_app.js
@@ -0,0 +1,23 @@
+import App from 'next/app';
+import Head from 'next/head';
+import { AppProvider } from '@shopify/polaris';
+
+class MyApp extends Component {
+    state = {  }
+    render() { 
+        const { Component, pageProps } = this.props;
+        return (  
+            <React.Fragment>
+                <Head>
+                    <title>Sample App</title>
+                    <meta charSet="utf-8" />
+                </Head>
+                <AppProvider>
+                    <Component {...pageProps} />
+                </AppProvider>
+            </React.Fragment>
+        );
+    }
+}
+ 
+export default MyApp;
