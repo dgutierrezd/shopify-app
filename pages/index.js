7ff1aed712b6146ec4c1d7f667fa3789219e8b39
Daniel Gutierrez Duque <danielgudu22@gmail.com>
2020-04-15 16:28:12 -0500

    adding and show products

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,8 @@
 import React, { useState } from "react";
 import { Page, Layout, EmptyState, TextStyle } from "@shopify/polaris";
 import { ResourcePicker, TitleBar } from "@shopify/app-bridge-react";
+import store from "store-js";
+import ResourceListWithProducts from "../components/ResourceList";
 
 const img = "https://cdn.shopify.com/s/files/1/0757/9955/files/empty-state.svg";
 
@@ -10,8 +12,10 @@ const Index = () => {
   const handleSelection = (resources) => {
     let idsFromResources = resources.selection.map((product) => product.id);
     setOpen(false);
-    console.log(idsFromResources);
+    store.set("ids", idsFromResources);
   };
+
+  const emptyState = !store.get("ids");
 
   return (
     <Page>
@@ -28,18 +32,22 @@ const Index = () => {
         onSelection={(resources) => handleSelection(resources)}
         onCancel={() => setOpen(false)}
       />
-      <Layout>
-        <EmptyState
-          heading="Discount your proucts temporarily"
-          action={{
-            content: "Select products",
-            onAction: () => setOpen(true),
-          }}
-          // image={img}
-        >
-          <p>Select products to change their price temprarily.</p>
-        </EmptyState>
-      </Layout>
+      {emptyState ? (
+        <Layout>
+          <EmptyState
+            heading="Discount your proucts temporarily"
+            action={{
+              content: "Select products",
+              onAction: () => setOpen(true),
+            }}
+            // image={img}
+          >
+            <p>Select products to change their price temprarily.</p>
+          </EmptyState>
+        </Layout>
+      ) : (
+        <ResourceListWithProducts />
+      )}
     </Page>
   );
 };
